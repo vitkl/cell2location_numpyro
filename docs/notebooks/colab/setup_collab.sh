45db40f2dd45b806c650854d01a761f2fabe056d
Vitalii Kleshchevnikov <vk7@sanger.ac.uk>
2020-12-05 11:23:14 +0000

    updated colab notebook

--- docs/notebooks/colab/setup_collab.sh
+++ docs/notebooks/colab/setup_collab.sh
@@ -3,7 +3,7 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -f -p /usr/local
 conda install -q -y --prefix /usr/local python=3.6 numpy pandas jupyter leidenalg python-igraph scanpy louvain hyperopt loompy cmake nose tornado dill ipython bbknn seaborn matplotlib request mkl-service pygpu theano --channel bioconda --channel conda-forge
 pip install -q --prefix /usr/local plotnine pymc3
-pip install -q --prefix /usr/local pip install --upgrade numpyro jax==0.2.3 jaxlib==0.1.56+cuda102 -f https://storage.googleapis.com/jax-releases/jax_releases.html
+pip install -q --prefix /usr/local --upgrade git+https://github.com/pyro-ppl/numpyro.git jax==0.2.3 jaxlib==0.1.56+cuda102 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 git clone https://github.com/BayraktarLab/cell2location.git cell2location_repo
 cd cell2location_repo && pip install -q --prefix /usr/local .
 git clone https://github.com/vitkl/cell2location_numpyro.git cell2location_numpyro
